3bb89ec986157313c2be45e9e2eb48b3efa65086
Shane Hopcroft <shanehopcroft@hotmail.com>
2021-09-24 21:58:30 +1000

    Initial upload

---
+++ Prime.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace prime
+{
+    public class Prime
+    {
+        //var _prime = new Prime();
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace prime
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
